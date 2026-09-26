2ecb5598f4abd8077f2c0dd37f02a4ab08cbd308
charo <zalireza829@gmail.com>
2026-09-26 12:37:09 +0330

    slove Exercise 2

--- topics/tuple/exercises/src/lib.rs
+++ topics/tuple/exercises/src/lib.rs
@@ -2,10 +2,9 @@
 pub fn first(t: (bool, u32, char)) -> bool {
     t.0
 }
-
-
-pub fn last(t: (bool, u32, char)) -> char {
-    todo!();
+// Exercise 2 
+pub fn last(t: (bool, u32, char)) -> char { 
+    t.2 
 }
 
 pub fn swap(t: (u32, u32)) -> (u32, u32) {
